3cb0a48605b2bf87271450952a6774472dacb7a7
Artem Rudenko <gvidon@ottofeller.com>
2021-06-18 09:10:51 +0200

    Add new rule that requires bumping the package version

---
+++ src/bump-package-version/__tests__/index.ts
@@ -0,0 +1,51 @@
+import {DangerDSLType, GitDSL, JSONDiff} from 'danger'
+import {bumpPackageVersion} from '../index'
+jest.mock('fs')
+
+describe('The rule that requires bumping the version in package.json in every pull request', () => {
+  it(
+    'compares the version in the PR diff and throws a message if the package.json:version has not changed',
+
+    async () => {
+      const includePaths = ['some-dir', 'some/nested/one']
+      const failMock = jest.fn()
+
+      await bumpPackageVersion({
+        danger: {
+          git: {
+            fileMatch: (path: string) => ({edited: path.startsWith(includePaths[1])}),
+
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
+            JSONDiffForFile: async (filename: string): Promise<JSONDiff> => ({
+              version: {added: [], after: '0.0.1', before: '0.0.1', removed: []},
+            }),
+          } as GitDSL,
+        } as DangerDSLType,
+
+        fail: failMock,
+        includePaths,
+      })
+
+      expect(failMock).toHaveBeenCalledTimes(1)
+      failMock.mockReset()
+
+      await bumpPackageVersion({
+        danger: {
+          git: {
+            fileMatch: (path: string) => ({edited: path.startsWith(includePaths[1])}),
+
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
+            JSONDiffForFile: async (filename: string): Promise<JSONDiff> => ({
+              version: {added: [], after: '0.0.1', before: '0.2.0', removed: []},
+            }),
+          } as GitDSL,
+        } as DangerDSLType,
+
+        fail: failMock,
+        includePaths,
+      })
+
+      expect(failMock).toHaveBeenCalledTimes(0)
+    },
+  )
+})
---
+++ src/bump-package-version/index.ts
@@ -0,0 +1,21 @@
+import {DangerDSLType} from 'danger'
+
+export const bumpPackageVersion = async (params: {
+  danger: DangerDSLType
+  fail: (message: string) => void
+  includePaths: Array<string>
+}) => {
+  // eslint-disable-next-line fp/no-loops
+  for(const includePath of params.includePaths) {
+    // If there are no edits in the includePath the version should not be bumped
+    if(!params.danger.git.fileMatch(`${includePath}/**/*`).edited) {
+      continue
+    }
+
+    const packageJsonVersionChange = (await params.danger.git.JSONDiffForFile(`${includePath}/package.json`))?.version
+
+    if(packageJsonVersionChange.after === packageJsonVersionChange.before) {
+      params.fail(`The version in package.json must be updated in ${includePath}`)
+    }
+  }
+}
