0d454f4c4b5ee9f5cf174d0ed5c89f1d7a6da19e
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-05-23 07:44:12 +0300

    Add optional excludePaths parameter to bumpPackageVersion rule
    In some projects there are files that should not trigger version update when edited. Eg. GraphQL schema.

--- src/bump-package-version/index.ts
+++ src/bump-package-version/index.ts
@@ -1,5 +1,5 @@
-import * as R from 'ramda'
 import {DangerDSLType} from 'danger'
+import * as R from 'ramda'
 
 /**
  * Checks all @includePaths and in case of presence of edited files
@@ -8,24 +8,38 @@ import {DangerDSLType} from 'danger'
  */
 export const bumpPackageVersion = async (params: {
   danger: DangerDSLType
+  excludePaths?: Array<string>
   fail: (message: string) => void
   includePaths: Array<string>
   restrictToBranches: Array<string>
 }) => {
-  if(!params.restrictToBranches.includes(params.danger.github.pr.base.ref)) {
+  if (!params.restrictToBranches.includes(params.danger.github.pr.base.ref)) {
     return
   }
 
-  R.forEach(async includePath => {
+  const files = params.danger.git.fileMatch(`**/*`)
+
+  if (!files.edited) {
+    return
+  }
+
+  const paths = R.compose(
+    R.reject(
+      R.anyPass(R.map<string, (list: string | readonly string[]) => boolean>(R.includes, params.excludePaths || [])),
+    ),
+    R.filter(R.anyPass(R.map(R.includes, params.includePaths))),
+  )(files.getKeyedPaths().edited) as string[]
+
+  R.forEach(async (path) => {
     // If there are no edits in the includePath the version should not be bumped
-    if(!params.danger.git.fileMatch(`${includePath}/**/*`).edited) {
+    if (!params.danger.git.fileMatch(`${path}/**/*`).edited) {
       return
     }
 
-    const packageJson = await params.danger.git.JSONDiffForFile(`${includePath}/package.json`)
+    const packageJson = await params.danger.git.JSONDiffForFile(`${path}/package.json`)
 
-    if(!packageJson || !packageJson.version || packageJson.version.after === packageJson.version.before) {
-      params.fail(`The version in package.json must be updated in ${includePath}`)
+    if (!packageJson || !packageJson.version || packageJson.version.after === packageJson.version.before) {
+      params.fail(`The version in package.json must be updated in ${path}`)
     }
-  }, params.includePaths)
+  }, paths)
 }
