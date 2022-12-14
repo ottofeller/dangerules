31e4c44bfdef328520beff197df510468f54afce
Roman Vasilyev <roman.vasilev@ottofeller.com>
2022-12-14 10:37:49 +0300

    Refactor tests to match updated nagSuppression rule (one warning per added suppression call)

--- src/cdk/__tests__/index.ts
+++ src/cdk/__tests__/index.ts
@@ -1,4 +1,5 @@
-import type {DangerDSLType, TextDiff} from 'danger'
+import type {DangerDSLType, StructuredDiff} from 'danger'
+import type {AddChange, Chunk, NormalChange} from 'parse-diff'
 import * as R from 'ramda'
 import * as utils from 'utils'
 import {nagSuppression} from '..'
@@ -10,12 +11,16 @@ describe('The rule that warns on every NagSuppressions.addResourceSuppressions()
   const notEdited = 'some/nested/not-edited'
   const includePaths = ['some-dir', edited, notEdited]
   const filteredPaths = R.map((folder) => `${folder}/index.ts`, includePaths)
+  const [, editedFilePath] = filteredPaths
+
+  const normalChange: NormalChange = {type: 'normal', ln1: 0, ln2: 0, normal: true, content: ''}
+  const emptyChunk: Chunk = {content: '', changes: [normalChange], oldStart: 0, oldLines: 0, newStart: 0, newLines: 0}
 
   const filterPathsMock = utils.filterPaths as jest.Mock<Array<string>, [params: utils.FilterParams]>
-  const diffForFileMock = jest.fn<Promise<TextDiff>, [filename: string]>()
+  const diffForFileMock = jest.fn<Promise<StructuredDiff>, [filename: string]>()
   const warnMock = jest.fn<void, [string]>()
 
-  const danger = {git: {diffForFile: diffForFileMock}} as unknown as DangerDSLType
+  const danger = {git: {structuredDiffForFile: diffForFileMock}} as unknown as DangerDSLType
 
   beforeEach(() => {
     filterPathsMock.mockReturnValue(filteredPaths)
@@ -55,7 +60,10 @@ describe('The rule that warns on every NagSuppressions.addResourceSuppressions()
 
   it('does not warn if no files contain "addResourceSuppressions" calls', async () => {
     const added = '// No suppressions added'
-    diffForFileMock.mockResolvedValue({before: '', after: added, diff: added, added, removed: ''})
+    const addChange: AddChange = {type: 'add', add: true, ln: 2, content: added}
+    const chunk: Chunk = {...emptyChunk, changes: emptyChunk.changes.concat(addChange)}
+
+    diffForFileMock.mockResolvedValue({chunks: [chunk]})
 
     await nagSuppression({danger, warn: warnMock, includePaths})
 
@@ -67,7 +75,9 @@ describe('The rule that warns on every NagSuppressions.addResourceSuppressions()
   it('warns on files containing "addResourceSuppressions" calls', async () => {
     diffForFileMock.mockImplementation(async (filename: string) => {
       const added = R.startsWith(edited)(filename) ? 'NagSuppressions.addResourceSuppressions(args)' : ''
-      return {before: '', after: added, diff: added, added, removed: ''}
+      const addChange: AddChange = {type: 'add', add: true, ln: 2, content: added}
+      const chunk: Chunk = {...emptyChunk, changes: emptyChunk.changes.concat(addChange)}
+      return {chunks: [chunk]}
     })
 
     await nagSuppression({danger, warn: warnMock, includePaths})
@@ -75,25 +85,40 @@ describe('The rule that warns on every NagSuppressions.addResourceSuppressions()
     expect(filterPathsMock).toHaveBeenCalledTimes(1)
     expect(diffForFileMock).toHaveBeenCalledTimes(3)
     expect(warnMock).toHaveBeenCalledTimes(1)
-    expect(warnMock).toHaveBeenCalledWith('Found 1 new NAG suppression calls', 'some/nested/edited/index.ts')
+    expect(warnMock).toHaveBeenCalledWith(
+      `Found new NAG suppression call in file "${editedFilePath}" at line ${2}`,
+      editedFilePath,
+      2,
+    )
   })
 
   it('reports multiple suppressions', async () => {
     diffForFileMock.mockImplementation(async (filename: string) => {
-      const added = R.startsWith(edited)(filename)
-        ? `NagSuppressions.addResourceSuppressions(args)
-          doSomething()
-          NagSuppressions.addResourceSuppressions(args)`
-        : ''
-
-      return {before: '', after: added, diff: added, added, removed: ''}
+      const added = R.startsWith(edited)(filename) ? 'NagSuppressions.addResourceSuppressions(args)' : ''
+      const addChange1: AddChange = {type: 'add', add: true, ln: 2, content: added}
+      const addChange2: AddChange = {type: 'add', add: true, ln: 7, content: added}
+      const chunk: Chunk = {...emptyChunk, changes: emptyChunk.changes.concat(addChange1, addChange2)}
+      return {chunks: [chunk]}
     })
 
     await nagSuppression({danger, warn: warnMock, includePaths})
 
     expect(filterPathsMock).toHaveBeenCalledTimes(1)
     expect(diffForFileMock).toHaveBeenCalledTimes(3)
-    expect(warnMock).toHaveBeenCalledTimes(1)
-    expect(warnMock).toHaveBeenCalledWith('Found 2 new NAG suppression calls', 'some/nested/edited/index.ts')
+    expect(warnMock).toHaveBeenCalledTimes(2)
+
+    expect(warnMock).toHaveBeenNthCalledWith(
+      1,
+      `Found new NAG suppression call in file "${editedFilePath}" at line ${2}`,
+      editedFilePath,
+      2,
+    )
+
+    expect(warnMock).toHaveBeenNthCalledWith(
+      2,
+      `Found new NAG suppression call in file "${editedFilePath}" at line ${7}`,
+      editedFilePath,
+      7,
+    )
   })
 })
